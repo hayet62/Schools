2e91f497a22c55207811730a4356d7e4af51b3e3
harousaidi8 <harounsaidi8@gmail.com>
2023-03-26 17:50:29 +0100

    hayet : feauture 2

--- app/src/main/java/com/wstechnologies/schools/MainActivity.kt
+++ app/src/main/java/com/wstechnologies/schools/MainActivity.kt
@@ -11,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<TextView>(R.id.txt1).setText("Haroun : feauture1")
+        findViewById<TextView>(R.id.txt2).setText("Hayet : feature2")
     }
 }
