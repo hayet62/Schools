0f3deb20ea56a2dd6b74369c808d05ef74dd9de4
harousaidi8 <harounsaidi8@gmail.com>
2023-03-26 17:39:58 +0100

    Initial commit

---
+++ app/src/main/java/com/wstechnologies/schools/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.wstechnologies.schools
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.txt1).setText("Haroun : feauture1")
+    }
+}
